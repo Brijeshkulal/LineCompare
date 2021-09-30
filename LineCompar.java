2733792b0194eb31f9407c2a6b482ff13d503b8b
“Brijesh” <brijeshkulal5@gmail.com>
2021-09-30 15:33:04 +0530

    Added length of the line

--- LineCompar.java
+++ LineCompar.java
@@ -3,16 +3,17 @@ public class LineCompar
 {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
-		double x1, x2, y1, y2, lengthOfLine;
+		int x1, x2, y1, y2;
+		double lengthOfLine;
 		Scanner in = new Scanner(System.in);
 		System.out.println("enter the point x1: ");
-		x1 = in.nextDouble();
+		x1 = in.nextInt();
 		System.out.println("enter the point x2: ");
-		x2 = in.nextDouble();
+		x2 = in.nextInt();
 		System.out.println("enter the point y1: ");
-		y1 = in.nextDouble();
+		y1 = in.nextInt();
 		System.out.println("enter the point y2: ");
-		y2 = in.nextDouble();
+		y2 = in.nextInt();
 		lengthOfLine = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 		System.out.println("Length of the line: " + lengthOfLine);
 	}
