4ffc4c3cbdef6b5abb814086292aa4df695373ea
“Brijesh” <brijeshkulal5@gmail.com>
2021-09-30 13:35:39 +0530

    calculated the legth of the line of the coordinates

--- LineCompar.java
+++ LineCompar.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
-public class LineCompar {
+public class LineCompar 
+{
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
-		double x1, x2, y1, y2;
-        double lengthOfLine;
-        Scanner in = new Scanner(System.in);
-        System.out.println("enter the point x1: ");
+		double x1, x2, y1, y2, lengthOfLine;
+		Scanner in = new Scanner(System.in);
+		System.out.println("enter the point x1: ");
 		x1 = in.nextDouble();
 		System.out.println("enter the point x2: ");
 		x2 = in.nextDouble();
@@ -13,7 +13,7 @@ public class LineCompar {
 		y1 = in.nextDouble();
 		System.out.println("enter the point y2: ");
 		y2 = in.nextDouble();
-        lengthOfLine = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-        System.out.println("Length of the line: " + lengthOfLine);
+		lengthOfLine = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		System.out.println("Length of the line: " + lengthOfLine);
 	}
 }
